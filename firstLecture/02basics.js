20e001f69a76c83552ce362c82b5740b54fcd6b8
prince mishra <87017487+Princemishra123@users.noreply.github.com>
2025-05-01 06:26:37 +0000

    datatypes

---
+++ firstLecture/02basics.js
@@ -0,0 +1,14 @@
+/* Data types*/
+
+"use strict" // treat all js code as new version
+let age = 18;
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. bigint
+// 5. null (stand alone value)
+// 6. undefined 
+// 7. symbol (To use for uniqueness)
+// 8. object
+console.log(typeof age);
+console.log(typeof null); // object//
