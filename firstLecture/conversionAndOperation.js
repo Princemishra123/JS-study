f1b01f811678756d2ce42ab8ea650f444f5291a3
prince mishra <87017487+Princemishra123@users.noreply.github.com>
2025-05-01 07:03:18 +0000

    operations

---
+++ firstLecture/conversionAndOperation.js
@@ -0,0 +1,28 @@
+// let age = 18
+// console.log(typeof(age)) //number
+
+let Age = "30"
+
+// console.log(Number(Age))
+
+let Dt = null;
+console.log(typeof Dt) // object
+// console.log(typeof Number(undefined)) // number
+let str = '30abc'
+
+console.log(Number(str)) // NaN but if we check type then it will be Number
+
+// console.log(typeof Number(str)) // number 
+
+// what we learn. , type of NaN is number 
+
+// console.log(Number(true)) // Output will be 1
+// console.log(Number(false)) // Output will be 0
+
+// console.log(Boolean(1)) // true
+// console.log(Boolean(0)) // false
+
+console.log('result',Boolean(-7)) // true
+console.log('result',Boolean(-0)) // false
+
+// "" => false, "Prince"=> true
